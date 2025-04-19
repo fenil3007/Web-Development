ba5a8c54a9ae2e1efe13ad90296f55dc8bf3515e
FENIL ITALIYA <151509519+fenil3007@users.noreply.github.com>
2025-04-19 14:03:44 +0530

    Web Scholar Portal Uploaded.
    This is the scholarship Portal using HTML and CSS.

---
+++ Web Scholar Portal/js/Onclick.js
@@ -0,0 +1,48 @@
+
+function Scholarship(){
+    window.open(
+        "Scholarships.html","_self"
+    );
+}
+function Educationloan(){
+    window.open(
+        "Educationloan.html","_self"
+    );
+}
+function Abroad(){
+    window.open(
+        "Abroad.html","_self"
+    );
+}
+function Home(){
+    window.open(
+        "Home.html","_self"
+    );
+};
+
+
+function About(){
+    window.open(
+        "About.html","_self"
+    );
+}
+function Contact(){
+    window.open(
+        "Contact.html","_self"
+    );
+}
+function Login(){
+    window.open(
+        "Login.html","_self"
+    );
+}
+function Register(){
+    window.open(
+        "Register.html","_self"
+    );
+}
+
+function alert1(){
+    window.alert("You have to need Log In to get Access of more feature.")
+}
+
